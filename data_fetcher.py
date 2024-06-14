c4fc76d71aca5e64394c1609b29c441609c4f3d8
Masterbaoguo <87047750+Masterbaoguo@users.noreply.github.com>
2024-06-14 16:15:37 +0800

    调整数据读取格式

--- data_fetcher.py
+++ data_fetcher.py
@@ -1,39 +1,47 @@
 import ccxt
 import pandas as pd
 import time
+import datetime
 
-# 定义一个获取所有OHLCV数据的函数
 def fetch_all_ohlcv(exchange, symbol, timeframe, since, rate_limit=True):
+    """
+    获取所有OHLCV数据
+    """
     all_bars = []
     while True:
-        # 获取数据并添加到列表
         bars = exchange.fetch_ohlcv(symbol, timeframe, since=since, limit=1000)
         if not bars:
             break
-        all_bars += bars
-        # 更新since为最后获取的时间戳 + 1毫秒
+        all_bars.extend(bars)
         since = bars[-1][0] + 1
-        # 如果启用限速需要等待一定时间
         if rate_limit:
             time.sleep(exchange.rateLimit / 1000)
     return all_bars
 
-# 定义一个函数来获取并保存OHLCV数据到CSV文件
 def fetch_and_save_ohlcv_to_csv(exchange, symbol, timeframe, from_ts, csv_filename):
+    """
+    获取并保存OHLCV数据到CSV文件
+    """
     all_bars = fetch_all_ohlcv(exchange, symbol, timeframe, from_ts)
     df = pd.DataFrame(all_bars, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
     df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms')
+    df.rename(columns={'timestamp': 'date', 'close': 'OT'}, inplace=True)
+    df = df[['date', 'open', 'high', 'low', 'volume', 'OT']]
     df.to_csv(csv_filename, index=False)
     print(f"Data saved to {csv_filename}")
 
-# 定义从CSV文件中读入数据的函数
 def read_data_from_csv(csv_filename):
-    df = pd.read_csv(csv_filename, parse_dates=['timestamp'])
-    df.set_index('timestamp', inplace=True)
+    """
+    从CSV文件中读入数据
+    """
+    df = pd.read_csv(csv_filename, parse_dates=['date'])
+    df.set_index('date', inplace=True)
     return df
 
-# 定义实时获取数据的函数
 def fetch_realtime_data(symbol, timeframe):
+    """
+    实时获取数据
+    """
     exchange = ccxt.binance()
     ohlcv = exchange.fetch_ohlcv(symbol, timeframe, limit=1000)
     data = pd.DataFrame(ohlcv, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
@@ -41,8 +49,8 @@ def fetch_realtime_data(symbol, timeframe):
     data.set_index('timestamp', inplace=True)
     return data
 
-# 用于下载数据的代码
 if __name__ == "__main__":
+    # 设置代理
     exchange = ccxt.binance({
         'proxies': {
             'http': 'http://127.0.0.1:4780',
@@ -50,13 +58,13 @@ if __name__ == "__main__":
         },
     })
 
-    # 选择交易对和时间框架
+    # 配置参数
     symbol = 'BTC/USDT'
     timeframe = '1m'
-    # 指定起始时间
-    from_ts = exchange.parse8601('2023-06-01 00:00:00')
-    # 设置CSV文件名
-    csv_filename = 'btc-2023-06-01.csv'
+    start_time_str = '2021-01-01 00:00:00'
+    from_ts = exchange.parse8601(start_time_str)
+    start_date_str = datetime.datetime.utcfromtimestamp(from_ts // 1000).strftime('%Y-%m-%d')
+    csv_filename = f'./dataset/btc/btc-{start_date_str}.csv'
 
     # 获取数据并保存到CSV文件
     fetch_and_save_ohlcv_to_csv(exchange, symbol, timeframe, from_ts, csv_filename)
